f9648b04684cc866838acebde4add3ff7d3295d6
Cody Thaller <stevencthaller@gmail.com>
2020-09-28 15:33:10 -0700

    Afternoon session

---
+++ LanguageFundamentals/BoxUnbox/Functions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BoxUnbox
+{
+    public class MyFunctions
+    {
+        public void MyFunction1()
+        {
+            System.Console.WriteLine("This is function 1 running!");
+        }
+    }
+}
---
+++ LanguageFundamentals/BoxUnbox/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> MyObjects = new List<object>
+            {
+                "String 1",
+                "String 2",
+                4,
+                'y',
+                false,
+                // "hello",
+                9
+            };
+
+
+
+            
+            if(MyObjects[2] is int && MyObjects[5] is int){
+                int sum = Convert.ToInt32(MyObjects[2]) + (int)MyObjects[5];
+                System.Console.WriteLine(sum);
+            } else {
+                System.Console.WriteLine("You can't add those two things together, they're not both numbers.");
+            }
+
+
+            Dictionary<string, object> MyDictionary = new Dictionary<string, object>();
+
+            MyDictionary.Add("FirstName", "Cody");
+            MyDictionary.Add("Age", 29);
+            MyDictionary.Add("HasDegree", true);
+
+            System.Console.WriteLine(MyDictionary["FirstName"]);
+
+            MyDictionary["FirstName"] = "Steven";
+
+            System.Console.WriteLine(MyDictionary["FirstName"]);
+
+
+
+            int[] number = {1,2,3,4,5};
+
+            Dictionary<string,string> dez = new Dictionary<string,string>();
+
+            foreach(var kvp in MyDictionary)
+            {
+                System.Console.WriteLine(kvp.Key + " - " + kvp.Value);
+            }
+
+            MyFunctions fun = new MyFunctions();
+
+            fun.MyFunction1();
+        }
+    }
+}
