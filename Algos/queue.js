0d6ce501bbd82c67eeed76aa14fbc4eda9e6234d
Cody Thaller <stevencthaller@gmail.com>
2020-10-06 08:52:31 -0700

    Queues

---
+++ Algos/queue.js
@@ -0,0 +1,67 @@
+const { SLNode, SLList } = require('./singly_linked_list');
+// A queue is a first in first out data structure.
+// Design a class to represent a queue using an array to store the items.
+// After that, design a class to represent a queue using a singly linked list
+// to store the items
+
+class ArrayQueue {
+    constructor(){
+        this.items = [];
+    }
+
+    // adds an item and returns the new size
+    enqueue(value){
+
+    }
+
+    // removes an item and returns it
+    dequeue(){
+
+    }
+
+    // returns a boolean based on whether or not the queue is empty
+    isEmpty(){
+
+    }
+
+    // returns the number of elements in the queue
+    size(){
+
+    }
+
+    // returns the first item without removing it
+    front(){
+        
+    }
+}
+
+class SLLQueue{
+    constructor(){
+        this.items = new SLList();
+    }
+
+    // adds an item and returns the new size
+    enqueue(value){
+
+    }
+
+    // removes an item and returns it
+    dequeue(){
+
+    }
+
+    // returns a boolean based on whether or not the queue is empty
+    isEmpty(){
+
+    }
+
+    // returns the number of elements in the queue
+    size(){
+
+    }
+
+    // returns the first item without removing it
+    front(){
+        
+    }
+}
