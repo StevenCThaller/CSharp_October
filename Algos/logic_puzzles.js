7145b3e92eecbaa93ae43d0ad3d0882ecef8c2e9
Cody Thaller <stevencthaller@gmail.com>
2020-10-23 08:33:48 -0700

    Whoops

--- Algos/logic_puzzles.js
+++ Algos/logic_puzzles.js
@@ -2,26 +2,13 @@
 // its total volume. You havea  running tap, and must use the containers and tap in such a way as to 
 // exactly measure out 4 gallons of water. How do??
 
-// 1. Fill the 5 gallon container
-// 2. Pour the contents 5 gallon container into the 3 gallon container (leaving 3 gallons in one container and 2 in the other)
-// 3. Empty the 3 gallon container, and pour the remaining 2 gallons from the 5 gallon container into the 3 gallon container
-// 4. Fill the 5 gallon container
-// 5. Pour contents of the 5 gallon container into the 3 gallon container to fill it up the rest of the way, leaving 3 gallons
-//    in the 3 gallon container, and 4 gallons in the 5 gallon container
 
 //                  _________
-//                  |       |                           |       |                           |       |        
-//                  |       |           _________       |       |                           |       |
-//  |       |       |       |  ==>      |       |       |_______|   ==>     |_______|       |       |   ==>
-//  |       |       |       |           |       |       |       |           |       |       |       |
-//  |_______|       |_______|           |_______|       |_______|           |_______|       |_______|
-
-//                  _________
-//                  |       |                           |_______|
-//                  |       |           _________       |       |
-//  |_______|       |       |   ==>     |       |       |       |
-//  |       |       |       |           |       |       |       |
-//  |_______|       |_______|           |_______|       |_______|
+//                  |       | 
+//                  |       | 
+//  |       |       |       | 
+//  |       |       |       | 
+//  |_______|       |_______| 
 
 
 
@@ -39,8 +26,8 @@
 
 
 
-// H  H T      T       |           
-//   T     T T         |                H   
-//      T   T          |             H       H
-//        H            |                 H
-//   H         T       |          
+// H  H T      T       
+//   T     T T         
+//      T   T          
+//        H            
+//   H         T       
