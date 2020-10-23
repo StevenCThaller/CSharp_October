4204ded59d02d035157cbde73dfd5651337d376a
Cody Thaller <stevencthaller@gmail.com>
2020-10-23 08:32:48 -0700

    Logic puzzles 10/23

--- Algos/doubly_linked_list.js
+++ Algos/doubly_linked_list.js
@@ -18,17 +18,45 @@ class DLList{
 
     // Write a method that will add to the front of our doubly linked list
     addToFront(value){
+        let newNode = new DLNode(value);
+        if(this.isEmpty()){
+            this.tail = newNode;
+            this.head = newNode;
+            return this;
+        }
 
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        return this;
     }
 
     // Write a method that will add to the back of our doubly linked list
     addToBack(value){
+        let newNode = new DLNode(value);
+        if(this.isEmpty()){
+            this.tail = newNode;
+            this.head = newNode;
+            return this;
+        }
 
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        return this;
     }
 
     // Write a method that will remove a node with a given value from our list.
-    // If no node with that value exists, return null. If that node is removed, return true
+    // If no node with that value exists, return false. If that node is removed, return true
     removeNode(value){
+        let headRun = this.head; 
+        let tailRun = this.tail;
+        while(headRun != tailRun && headRun.prev != tailRun) {
+            if(headRun.value == value){
 
+            } else if(tailRun.value == value){
+
+            }
+        }
     }
 }
---
+++ Algos/logic_puzzles.js
@@ -0,0 +1,46 @@
+// You have a 3 and a 5 gallon water container. Each container has no markings except for text displaying 
+// its total volume. You havea  running tap, and must use the containers and tap in such a way as to 
+// exactly measure out 4 gallons of water. How do??
+
+// 1. Fill the 5 gallon container
+// 2. Pour the contents 5 gallon container into the 3 gallon container (leaving 3 gallons in one container and 2 in the other)
+// 3. Empty the 3 gallon container, and pour the remaining 2 gallons from the 5 gallon container into the 3 gallon container
+// 4. Fill the 5 gallon container
+// 5. Pour contents of the 5 gallon container into the 3 gallon container to fill it up the rest of the way, leaving 3 gallons
+//    in the 3 gallon container, and 4 gallons in the 5 gallon container
+
+//                  _________
+//                  |       |                           |       |                           |       |        
+//                  |       |           _________       |       |                           |       |
+//  |       |       |       |  ==>      |       |       |_______|   ==>     |_______|       |       |   ==>
+//  |       |       |       |           |       |       |       |           |       |       |       |
+//  |_______|       |_______|           |_______|       |_______|           |_______|       |_______|
+
+//                  _________
+//                  |       |                           |_______|
+//                  |       |           _________       |       |
+//  |_______|       |       |   ==>     |       |       |       |
+//  |       |       |       |           |       |       |       |
+//  |_______|       |_______|           |_______|       |_______|
+
+
+
+
+
+
+
+
+// You have 50 coins. 4 of them are heads up, and the rest are heads down. You cannot see them, and you cannot
+// feel any difference between heads or tails. How can you split up the coins into two groups so that each 
+// group of coins has the same number of heads?
+
+
+
+
+
+
+// H  H T      T       |           
+//   T     T T         |                H   
+//      T   T          |             H       H
+//        H            |                 H
+//   H         T       |          
